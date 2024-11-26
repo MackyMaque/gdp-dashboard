4bdc181c6649af77d0fb4122edf8072edadf113b
MackyMaque <maquejeryllem@gmail.com>
2024-11-26 14:04:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 def display_title(app_title, subtitle):
     
@@ -14,3 +15,17 @@ st.set_page_config(page_title="Title Template Example", layout="centered")
 
 display_title("MAQUE, JERYLLE ", "BSCpE-1B")
 
+st.set_page_config(page_title="Picture Uploader", layout="centered")
+
+st.title("Upload Your Picture")
+
+# File uploader widget
+uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    # Open and display the uploaded image
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.success("Image uploaded successfully!")
+else:
+    st.info("Please upload an image file to see it displayed here.")
